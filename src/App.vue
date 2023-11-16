064122c9a71d23ea88f12de3cab61cd87b70d33b
devs1k0 <devs1ko.dev@gmail.com>
2023-11-16 22:55:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>SITE UNDER CONSTRUCTION</h1>
+    <a href="https://github.com/Robbna/Robbna"> </a>
+  </div>
+</template>
+<style scoped>
+div {
+  font-family: system-ui;
+  font-weight: 300;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+}
+</style>
